abf621637765b4e6603da1079af37234b2908b4a
tjr226 <tjr226@gmail.com>
2019-04-10 11:29:39 -0400

    article.js expand functionality implemented

--- Article/Article.js
+++ Article/Article.js
@@ -9,12 +9,16 @@ class Article {
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent += "Expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener('click', () => this.expandArticle());
+    // explicit binding
+    this.expandButton.addEventListener('click', this.expandArticle.bind(this));
+
+    // not explicit binding
+    // this.expandButton.addEventListener('click', () => expandArticle());
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.article.classList.toggle('.article-open')
+    this.article.classList.toggle('article-open');
   }
 }
 
